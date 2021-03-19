adf0929a9a7e9adad7419ea4b7d8e29f97548563
Alexandr Karpov <alexandr.d.karpov@gmail.com>
2021-03-19 13:58:29 -0400

    final (qraphal test)

--- log_client.py
+++ log_client.py
@@ -1,4 +1,5 @@
 import socket
+from python_graphql_client import GraphqlClient
 
 sock = socket.socket()
 sock.bind(('', 7777))
@@ -8,6 +9,23 @@ while True:
     conn, addr = sock.accept()
     print('connected:', addr)
     conn.send('Ok'.encode()) #sends key
-    inf = conn.recv(8192).decode() #gets encoded information
-    print(inf)
+    inf = conn.recv(1024).decode() #gets encoded id
+    adr = conn.recv(1024).decode() #gets encoded adress
+     graphql_connect(id)
     conn.close()
+
+def graphql_con(id):
+    client = GraphqlClient(endpoint)
+    query = """
+    query countryQuery($user_data: String) {
+        user_data(id:$id) {
+            id
+            name
+            role
+        }
+    }
+    """
+    variables = {"id": id}
+
+    data = client.execute(query=query, variables=variables)
+    return data
--- log_server.py
+++ log_server.py
@@ -15,11 +15,7 @@ while True:
 def send_to_log(id, adr):
     sock.connect(('localhost', 4040))
     data = sock.recv(1024).decode() # recev key
-    string = 'Error with ' + str(id) + ' in ' + str(adr) 
-    if adr == 'park ip':
-        string = 'Entered: ' + str(id) + ' from park'
-    elif adr == 'enter ip':
-        string = 'Entered: ' + str(id) + ' from enter'
-    sock.send(string.encode())
+    sock.send(id)
+    sock.send(adr)
     sock.close()
     return 0
